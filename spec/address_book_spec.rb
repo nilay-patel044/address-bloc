bc8e879e342d1f2478e41d3968ef0271829e5955
nilay-patel044 <nilaypatel044@gmail.com>
2017-10-26 00:10:26 -0400

    Assignment-4: Completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,4 +37,21 @@ RSpec.describe AddressBook do
          expect(new_entry.email).to eq('augusta.king@lovelace.com')
        end
      end
+
+     describe "#remove_entry" do
+       it "removes an entry using the name, phone_number, and email address" do
+         book = AddressBook.new
+         book.add_entry('Nilay Patel', '010.210.5181', 'nilay.patel@nilaypatel.tech')
+
+         name = 'Ada Lovelace'
+         phone_number = '010.012.1815'
+         email = 'augusta.king@lovelace.com'
+         book.add_entry(name, phone_number, email)
+
+         expect(book.entries.size).to eq 2
+         book.remove_entry(name, phone_number, email)
+         expect(book.entries.size).to eq 1
+         expect(book.entries.first.name).to eq('Nilay Patel')
+       end
+     end
    end
