26b71afb91b2673c9b8db1265d9f02cf40a2d391
nilay-patel044 <nilaypatel044@gmail.com>
2017-10-30 21:48:46 -0400

    Assignment-7: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -5,7 +5,7 @@ class AddressBook
   attr_reader :entries
 
   def initialize
-  @entries = []
+    @entries = []
   end
 
   def add_entry(name, phone_number, email)
@@ -30,5 +30,39 @@ class AddressBook
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    # #2
+    while lower <= upper
+      # #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    # #5
+    return nil
+  end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+
+    return nil
   end
 end
